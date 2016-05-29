f07146b6a8a62e75c88367544a765941dcd7d9d0
rcsonka <rcsonka@users.noreply.github.com>
2016-05-29 17:04:15 +0200

    Update ProduktDAO.java

--- ApothekeOnline/src/dao/ProduktDAO.java
+++ ApothekeOnline/src/dao/ProduktDAO.java
@@ -3,7 +3,19 @@ package dao;
 import java.util.List;
 
 import model.Produkt;
+import model.Category;
 
 public interface ProduktDAO {
+	public boolean speichereProdukt(Produkt p);
+	public boolean speichereCategory(Category c);
+	
+	public List<Produkt> getProduktList();
+	public List<Category> getCategoryList();
+	public Produkt getProduktByProduktID(int prodID);
+	public Category getCategoryByCategoryID(int categoryID);
+	
+    
+	public boolean loescheProduktByProdID(prodID);
+	public boolean loescheCategoryByCategoryID(categoryID);
 	
 }
